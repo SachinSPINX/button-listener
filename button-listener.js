4813ae663ee71bc714fdd8d9daf297a3f698b7b7
Sachin GTM <sachin.spinx@gmail.com>
2024-09-11 18:11:17 +0530

    Create button-listener.js

---
+++ button-listener.js
@@ -0,0 +1,27 @@
+document.querySelector('.btn-seo-contact1').addEventListener('click', function() {
+    alert('hello');
+});
+
+//this above will give an alert with the click
+
+--------------------------------------------------------------------------------
+  
+document.querySelector('.btn-seo-contact1').addEventListener('click', function() {
+    alert('hello');
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'contact_button_click'
+    });
+});
+
+//
+
+---------------------------------------------------------------------------------
+document.querySelector('.btn-seo-contact1').addEventListener('click', function() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'contact_button_click'
+    });
+});
+
+// this will work when someone will click on the button that class is mentioned in the scripe and will send the DL .
